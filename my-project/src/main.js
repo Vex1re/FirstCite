b75c0b2a822468647e38db342993659d4702c67c
Vex1re <ivanovmatvej4548@gmail.com>
2026-01-22 20:43:45 +0400

    About Me + Hero landing

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,1 +1,19 @@
 import './style.css'
+
+const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 240;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
