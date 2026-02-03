c5ac7bc1f35dc61fe9a3107c0cd2dcf07ecc7820
Vex1re <ivanovmatvej4548@gmail.com>
2026-02-03 17:46:45 +0400

    Why Me + Project + navbar changes

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -36,7 +36,7 @@ window.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove(
       "py-3",
-      "backdrop-blur-xl",
+      "backdrop-blur-xs",
       "bg-red-950/80",
       "shadow-[0_20px_60px_rgba(0,0,0,0.7)]"
     );
@@ -44,3 +44,44 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// ТЕСТ НАКЛОНА КАРТОЧЕК
+
+const card = document.getElementById("tilt-card");
+
+card.addEventListener('mousemove', (e) => {
+  const rect = card.getBoundingClientRect();
+
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = (centerY - y) / 10;
+  const rotateY = (x - centerX) / 10;
+
+  card.style.transform = 'perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.05, 1.05, 1.05)';
+});
+
+card.addEventListener('mouseleave', () => {
+  card.style.transform = "perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)";
+});
+
+// ТЕСТ БЕСКОНЕЧНОГО СКРОЛЛБАРА
+
+const slider = document.getElementById("track");
+const firstRealCard = slider.children[1];
+slider.scrollLeft = firstRealCard.offsetLeft - slider.offsetLeft;
+
+slider.addEventListener('scroll', () => {
+  const scrollPos = slider.scrollLeft;
+  const maxScroll = slider.scrollWidth - slider.clientWidth;
+
+  if (scrollPos >= maxScroll - 5){
+    slider.scrollLeft = sliderchildren[1].offsetLeft - slider.offsetLeft;
+  }
+
+  if (scrollPos <= 5){
+    slider.scrollLeft - slider.children[slider.children.length - 2].offsetLeft - slider.offsetLeft;
+}
+});
